498fe5729aa3c789833cb0c6391b2bd5874d8ef3
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-07-07 16:35:10 -1000

    delete comment

--- vault.js
+++ vault.js
@@ -1,22 +1,20 @@
-'use strict';
+"use strict";
 module.exports = function() {
   let myVault = {};
-  function setValue (key, value){
+  function setValue(key, value) {
     myVault[key] = value;
   }
 
-  function getValue (key){
-    if(!key || !myVault[key]){
+  function getValue(key) {
+    if (!key || !myVault[key]) {
       return null;
-    }else if(myVault[key]){
+    } else if (myVault[key]) {
       return myVault[key];
     }
   }
 
   return {
-    setValue:setValue,
-    getValue:getValue
-  }
+    setValue: setValue,
+    getValue: getValue
+  };
 };
-
-//how can I test these? the console log example in the read me isn't working for me.
